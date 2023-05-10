b10386616c433243fe15ef8830c6062949b4f7d9
Foxymansama <dimalatoho1448@gmail.com>
2023-05-10 08:45:39 +0300

    Getted a new answer

--- leetcode.hpp
+++ leetcode.hpp
@@ -1,6 +1,80 @@
-class Solution200 {
-    class UnionFind {
-    public:
-    };
+#ifndef LEETCODE_HPP
+#define LEETCODE_HPP
 
+#include <memory>
+#include <vector>
+
+template<
+    typename T = int>
+using Vec2D = std::vector<std::vector<T>>;
+
+class Solution695 {
+public:
+    class UnionFind {
+        
+    };
 };
+class Solution1971 {
+public:
+    // Accepted
+    class UnionFind {
+    private:
+        std::vector<int> id_;
+        std::vector<int> sz_;
+
+    public:
+        void toId(int _n,
+                  const std::vector<std::vector<int>> &_edges) {
+            id_.resize(_n);
+            sz_.resize(_n);
+            for (size_t i { }; i < _n; ++i) {
+                id_[i] = i;
+                sz_[i] = 1;
+            }
+
+            size_t size { _edges.size() };
+            for (size_t i { }; i < size; ++i) {
+                int p_root { find(_edges[i][0]) };
+                int q_root { find(_edges[i][1]) };
+                if (p_root == q_root) {
+                    continue;
+                }
+                if (sz_[p_root] > sz_[q_root]) {
+                    id_[q_root] = p_root;
+                    sz_[p_root] += sz_[q_root];
+                }
+                else {
+                    id_[p_root] = q_root;
+                    sz_[q_root] += sz_[p_root];
+                }
+            }
+        }
+
+        int find(int _p) {    
+            int i { _p };
+            for (; i != id_[i]; i = id_[i]);
+            // Path compression
+            while (id_[_p] != i) {
+                int parent { id_[_p] };
+                id_[_p] = i;
+                _p = parent;
+            }
+
+            return i;
+        }
+        bool validPath(int _n,
+                       std::vector<std::vector<int>> &_edges,
+                       int _source,
+                       int _dest) {
+            toId(_n, _edges);
+            if (find(_source) == find(_dest)) {
+                return true;
+            }
+
+            return false;
+        }
+    };
+};
+
+#endif
+
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "leetcode.hpp"
 
 int main() {
-
+    Solution1971::UnionFind test { };
+    Vec2D<int> vec { { 4, 3 }, { 1, 4 }, { 4, 8 }, { 1, 7 }, { 6, 4 }, { 4, 2 }, { 7, 4 }, { 4, 0 }, { 0, 9 }, { 5, 4 } };
+    test.validPath(10, vec, 5, 9);
+    system("pause");
 }
